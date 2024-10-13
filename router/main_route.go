df815f4bf90f918336d08d80ac128d3ef2dc077e
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-10-13 12:55:44 +0700

    feat: create route

---
+++ router/main_route.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/rizky201008/subscription-tracker-api/models/web"
+)
+
+func MainRouter(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{})
+	})
+	api := app.Group("/api")
+	api.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(web.Response{
+			StatusCode: web.RcSuccess,
+			Data:       nil,
+			Message:    "Sukses",
+		})
+	})
+}
