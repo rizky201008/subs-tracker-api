3106916a00bde4d0f2d30e5cabeec2b93c4297c5
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-10-13 12:26:16 +0700

    feat: initialize gorm

---
+++ config/gorm.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/rizky201008/subscription-tracker-api/models/domain"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var Db *gorm.DB
+
+func InitDb() {
+	host := Vipers.GetString("db.host")
+	port := Vipers.GetString("db.port")
+	user := Vipers.GetString("db.user")
+	password := Vipers.GetString("db.password")
+	database := Vipers.GetString("db.database")
+
+	dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + database + "?parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect databases" + err.Error())
+	}
+	err = db.AutoMigrate(&domain.Setting{}, &domain.Transaction{})
+	if err != nil {
+		panic(err.Error())
+	}
+	Db = db
+}
