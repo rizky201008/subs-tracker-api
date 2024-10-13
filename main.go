6be4087150da37c1f73750d9cfb2cfbe2a440d76
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-10-13 12:57:24 +0700

    feat: create main file

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/rizky201008/subscription-tracker-api/config"
+	"github.com/rizky201008/subscription-tracker-api/router"
+)
+
+func init() {
+	config.InitViper()
+	config.InitDb()
+}
+
+func main() {
+	app := fiber.New()
+
+	router.MainRouter(app)
+
+	err := app.Listen(config.Vipers.GetString("app.port"))
+	if err != nil {
+		panic(err)
+	}
+}
