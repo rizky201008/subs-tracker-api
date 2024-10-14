dcb0585e9645b8a0a943a4dd401eb40909ca0c28
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-10-14 22:54:15 +0700

    refactor: model = add gorm.Model

--- models/domain/setting.go
+++ models/domain/setting.go
@@ -1,6 +1,9 @@
 package domain
 
+import "gorm.io/gorm"
+
 type Setting struct {
+	gorm.Model
 	UserID   string `gorm:"column:user_id;type:text"`
 	Currency string `gorm:"column:currency;type:varchar(5);default:USD;"`
 }
