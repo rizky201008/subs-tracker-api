47f93d59d0a39575fadd91df2f650170d102c0a9
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-10-22 15:19:04 +0700

    feat: controller = add methods

--- controllers/setting_controller.go
+++ controllers/setting_controller.go
@@ -1,1 +1,42 @@
 package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/rizky201008/subscription-tracker-api/models/web"
+	"github.com/rizky201008/subscription-tracker-api/services"
+)
+
+type SettingController interface {
+	GetSetting(ctx *fiber.Ctx) error
+	UpdateSetting(ctx *fiber.Ctx) error
+	CreateSetting(ctx *fiber.Ctx) error
+}
+
+type SettingControllerImpl struct {
+	SettingService services.SettingService
+}
+
+func NewSettingController(settingService services.SettingService) SettingControllerImpl {
+	return SettingControllerImpl{
+		SettingService: settingService,
+	}
+}
+
+func (controller SettingControllerImpl) GetSetting(ctx *fiber.Ctx) error {
+	response := web.NewResponse()
+	response.Data = controller.SettingService.GetSetting(ctx)
+
+	return ctx.JSON(response)
+}
+
+func (controller SettingControllerImpl) UpdateSetting(ctx *fiber.Ctx) error {
+	response := web.NewResponse()
+	response.Data = controller.SettingService.UpdateSetting(ctx)
+	return ctx.JSON(response)
+}
+
+func (controller SettingControllerImpl) CreateSetting(ctx *fiber.Ctx) error {
+	response := web.NewResponse()
+	response.Data = controller.SettingService.CreateSetting(ctx)
+	return ctx.JSON(response)
+}
