18a05c2cf0f0a6486935da7e0d64693d14e8d6ac
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-10-27 22:46:53 +0700

    feat: config = create new initiator for repository and service

--- config/services.go
+++ config/services.go
@@ -3,7 +3,12 @@ package config
 import "github.com/rizky201008/subscription-tracker-api/services"
 
 var SettingService services.SettingService
+var SubscriptionService services.SubscriptionService
 
 func InitSettingService() {
 	SettingService = services.NewSettingService(Db, SettingRepository)
+}
+
+func InitSubscriptionService() {
+	SubscriptionService = services.NewSubscriptionService(Db, SubscriptionRepository)
 }
